8d25b70adc8e797b20e0d60c9ac0770f2c52489f
metara <jeff.j.lu2@gmail.com>
2023-05-23 01:10:57 +1000

    something

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello there");
+console.log("Hello there");
+
+const button = document.querySelector("button");
+
+function get_input() {
+    const num = prompt("What is your favourite number?");
+    var answer = "";
+    for (var i = 2; i < num; i++) {
+        if (num % i == 0) {
+            answer = answer + i + ',';
+        }
+    }
+    alert(answer);
+}
+
+button = document.getElementById()
